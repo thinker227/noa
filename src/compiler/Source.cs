bf416c8d5ce69fc8a32daeb038dccffa809041c5
thinker227 <thinker0227@gmail.com>
2024-02-03 23:57:14 +0100

    Add docs to Source

--- src/compiler/Source.cs
+++ src/compiler/Source.cs
@@ -1,3 +1,8 @@
 namespace Noa.Compiler;
 
+/// <summary>
+/// Represents a named piece of source text, eg. a source file.
+/// </summary>
+/// <param name="Text">The text of the source.</param>
+/// <param name="Name">The name of the source.</param>
 public readonly record struct Source(string Text, string Name);
