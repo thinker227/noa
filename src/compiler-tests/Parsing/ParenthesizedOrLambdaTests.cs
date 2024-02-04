87329d4401e62fb32fe86659fde552dca61cd2c2
thinker227 <thinker0227@gmail.com>
2024-02-04 23:48:49 +0100

    Update ParenthesizedOrLambdaTests

--- src/compiler-tests/Parsing/ParenthesizedOrLambdaTests.cs
+++ src/compiler-tests/Parsing/ParenthesizedOrLambdaTests.cs
@@ -5,11 +5,34 @@ namespace Noa.Compiler.Parsing.Tests;
 public class ParenthesizedOrLambdaTests
 {
     [Fact]
+    public void Parses_EmptyList_WithArrow_AsLambda()
+    {
+        using var p = ParseAssertion.Create(
+            "() => 0",
+            p => p.ParseParenthesizedOrLambdaExpression());
+
+        p.N<LambdaExpression>();
+        {
+            p.N<NumberExpression>(n => n.Value.ShouldBe(0));
+        }
+    }
+
+    [Fact]
+    public void Parses_EmptyList_WithoutArrow_AsError()
+    {
+        using var p = ParseAssertion.Create(
+            "()",
+            p => p.ParseParenthesizedOrLambdaExpression());
+
+        p.N<ErrorExpression>();
+    }
+    
+    [Fact]
     public void Parses_MultipleNameList_WithArrow_AsLambda()
     {
-        var p = ParseAssertion.Create("""
-        (a, b, c) => 0
-        """, p => p.ParseParenthesizedOrLambdaExpression());
+        using var p = ParseAssertion.Create(
+            "(a, b, c) => 0",
+            p => p.ParseParenthesizedOrLambdaExpression());
 
         p.N<LambdaExpression>();
         {
@@ -35,9 +58,9 @@ public class ParenthesizedOrLambdaTests
     [Fact]
     public void Parses_MultipleNameList_WithoutArrow_AsTuple()
     {
-        var p = ParseAssertion.Create("""
-        (a, b, c)
-        """, p => p.ParseParenthesizedOrLambdaExpression());
+        using var p = ParseAssertion.Create(
+            "(a, b, c)",
+            p => p.ParseParenthesizedOrLambdaExpression());
 
         p.N<TupleExpression>();
         {
@@ -52,9 +75,9 @@ public class ParenthesizedOrLambdaTests
     [Fact]
     public void Parses_SingleNameList_WithArrow_AsLambda()
     {
-        var p = ParseAssertion.Create("""
-        (x) => 0
-        """, p => p.ParseParenthesizedOrLambdaExpression());
+        using var p = ParseAssertion.Create(
+            "(x) => 0",
+            p => p.ParseParenthesizedOrLambdaExpression());
 
         p.N<LambdaExpression>();
         {
@@ -70,10 +93,75 @@ public class ParenthesizedOrLambdaTests
     [Fact]
     public void Parses_SingleNameList_WithoutArrow_AsParenthesized()
     {
-        var p = ParseAssertion.Create("""
-        (x)
-        """, p => p.ParseParenthesizedOrLambdaExpression());
+        using var p = ParseAssertion.Create(
+            "(x)",
+            p => p.ParseParenthesizedOrLambdaExpression());
 
         p.N<IdentifierExpression>(i => i.Identifier.ShouldBe("x"));
+    }
+    
+    [Fact]
+    public void Parses_SingleCallExpression_WithoutArrow_AsParenthesized()
+    {
+        using var p = ParseAssertion.Create(
+            "(f())",
+            p => p.ParseParenthesizedOrLambdaExpression());
+
+        p.N<CallExpression>();
+        {
+            p.N<IdentifierExpression>(i => i.Identifier.ShouldBe("f"));
+        }
+    }
+    
+    [Fact]
+    public void Parses_SingleBinaryExpression_WithoutArrow_AsParenthesized()
+    {
+        using var p = ParseAssertion.Create(
+            "(a + b)",
+            p => p.ParseParenthesizedOrLambdaExpression());
+
+        p.N<BinaryExpression>(b => b.Kind.ShouldBe(BinaryKind.Plus));
+        {
+            p.N<IdentifierExpression>(i => i.Identifier.ShouldBe("a"));
+            
+            p.N<IdentifierExpression>(i => i.Identifier.ShouldBe("b"));
+        }
+    }
+
+    [Fact]
+    public void Parses_MutName_ThenExpression_WithArrow_AsLambda()
+    {
+        using var p = ParseAssertion.Create(
+            "(mut x, 0) => 1",
+            p => p.ParseParenthesizedOrLambdaExpression());
+
+        p.N<LambdaExpression>();
+        {
+            p.N<Parameter>(param => param.IsMutable.ShouldBeTrue());
+            {
+                p.N<Identifier>(i => i.Name.ShouldBe("x"));
+            }
+            
+            // 0 should not be parsed as anything.
+
+            p.N<NumberExpression>(n => n.Value.ShouldBe(1));
+        }
+    }
+
+    [Fact]
+    public void Parses_Expression_ThenMutName_WithoutArrow_AsTuple()
+    {
+        using var p = ParseAssertion.Create(
+            "(0, mut x)",
+            p => p.ParseParenthesizedOrLambdaExpression());
+
+        p.N<TupleExpression>();
+        {
+            p.N<NumberExpression>(n => n.Value.ShouldBe(0));
+            
+            // mut should not be parsed as anything.
+
+            p.N<IdentifierExpression>(i => i.Identifier.ShouldBe("x"));
+        }
     }
 }
