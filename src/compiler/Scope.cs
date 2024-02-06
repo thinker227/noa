c0c60b3e03e7000da993d9b5a5c80da2cff7c796
thinker227 <thinker0227@gmail.com>
2024-02-06 16:56:10 +0100

    Add BlockedScope

--- src/compiler/Scope.cs
+++ src/compiler/Scope.cs
@@ -6,7 +6,7 @@ namespace Noa.Compiler;
 /// A semantic scope containing named symbols.
 /// </summary>
 /// <param name="parent">The parent scope, or null if the scope is the global scope.</param>
-public sealed class Scope(Scope? parent)
+public class Scope(Scope? parent)
 {
     private readonly Dictionary<string, ISymbol> symbols = new();
     
@@ -17,26 +17,31 @@ public sealed class Scope(Scope? parent)
 
     /// <summary>
     /// The symbols declared in the scope.
+    /// These symbols are always completely accessible.
     /// </summary>
     public IReadOnlyDictionary<string, ISymbol> DeclaredSymbols => symbols;
 
     /// <summary>
-    /// The symbols accessible within the scope.
+    /// The symbols accessible within the scope, as well as their accessibility.
     /// </summary>
-    public IEnumerable<ISymbol> AccessibleSymbols =>
-        DeclaredSymbols.Values.Concat(Parent?.AccessibleSymbols ?? []);
+    public virtual IEnumerable<(ISymbol, SymbolAccessibility)> AccessibleSymbols =>
+        DeclaredSymbols.Values
+            .Select(s => (s, SymbolAccessibility.Accessible))
+            .Concat(Parent?.AccessibleSymbols ?? []);
     
     /// <summary>
     /// Looks up a symbol with a specified name in the scope.
     /// </summary>
     /// <param name="name">The name of the symbol to look up.</param>
-    /// <param name="predicate">A predicate which determines whether to return
-    /// a given symbol with the specified name.</param>
-    /// <returns>The found symbol, or null if it could not be found.</returns>
-    public ISymbol? LookupSymbol(string name, Func<ISymbol, bool>? predicate = null)
+    /// <param name="predicate">
+    /// A predicate which determines whether to return a given symbol with the specified name.
+    /// </param>
+    /// <returns>The found symbol as well as its accessibility, or null if no symbol could not be found.</returns>
+    public virtual (ISymbol, SymbolAccessibility)? LookupSymbol(string name, Func<ISymbol, bool>? predicate = null)
     {
-        if (symbols.TryGetValue(name, out var symbol) && (predicate?.Invoke(symbol) ?? true))
-            return symbol;
+        if (symbols.TryGetValue(name, out var symbol) &&
+            (predicate?.Invoke(symbol) ?? true))
+            return (symbol, SymbolAccessibility.Accessible);
 
         return Parent?.LookupSymbol(name, predicate);
     }
@@ -47,4 +52,43 @@ public sealed class Scope(Scope? parent)
     /// <param name="symbol">The symbol to declare.</param>
     internal void Declare(ISymbol symbol) =>
         symbols[symbol.Name] = symbol;
+}
+
+/// <summary>
+/// A scope which returns <see cref="SymbolAccessibility.Blocked"/> for symbols in its parent scope.
+/// </summary>
+/// <param name="parent">The parent scope, or null if the scope is the global scope.</param>
+public sealed class BlockingScope(Scope? parent) : Scope(parent)
+{
+    public override IEnumerable<(ISymbol, SymbolAccessibility)> AccessibleSymbols =>
+        DeclaredSymbols.Values
+            .Select(s => (s, SymbolAccessibility.Accessible))
+            .Concat(Parent?.AccessibleSymbols
+                .Select(s => (s.Item1, SymbolAccessibility.Blocked))
+                ?? []);
+
+    public override (ISymbol, SymbolAccessibility)? LookupSymbol(string name, Func<ISymbol, bool>? predicate = null)
+    {
+        if (DeclaredSymbols.TryGetValue(name, out var symbol) &&
+            (predicate?.Invoke(symbol) ?? true))
+            return (symbol, SymbolAccessibility.Accessible);
+
+        if (Parent?.LookupSymbol(name, predicate) is not var (parentSymbol, _)) return null;
+        return (parentSymbol, SymbolAccessibility.Blocked);
+    }
+}
+
+/// <summary>
+/// Defines how a symbol is accessible.
+/// </summary>
+public enum SymbolAccessibility
+{
+    /// <summary>
+    /// The symbol is accessible as normal.
+    /// </summary>
+    Accessible,
+    /// <summary>
+    /// The symbol is accessible in a parent scope but access to it is blocked.
+    /// </summary>
+    Blocked,
 }
