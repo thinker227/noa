8d8863811886c76af6fb24c4880962f2703b229e
thinker227 <thinker0227@gmail.com>
2024-02-08 21:34:57 +0100

    Update BlockingScope

--- src/compiler/Symbols/BlockingScope.cs
+++ src/compiler/Symbols/BlockingScope.cs
@@ -10,20 +10,23 @@ internal sealed class BlockingScope(IScope parent, Node declaration) : IScope
     public IScope Parent { get; } = parent;
 
     // Blocking scopes do not declare symbols of their own, they only forward symbols from their parent scope.
+
+    private static LookupResult MapLookup(LookupResult lookup) => lookup.Symbol switch
+    {
+        VariableSymbol or ParameterSymbol => lookup with { Accessibility = SymbolAccessibility.Blocked },
+        _ => lookup
+    };
     
     public LookupResult? LookupSymbol(string name, Node? at, Func<ISymbol, bool>? predicate = null)
     {
         if (Parent.LookupSymbol(name, declaration, predicate) is not { } lookup) return null;
 
-        return lookup.Symbol switch
-        {
-            VariableSymbol or ParameterSymbol => lookup with { Accessibility = SymbolAccessibility.Blocked },
-            _ => lookup
-        };
+        return MapLookup(lookup);
     }
     
     public IEnumerable<IDeclaredSymbol> DeclaredAt(Node? at) => [];
 
     public IEnumerable<LookupResult> AccessibleAt(Node? at) =>
-        Parent.AccessibleAt(declaration);
+        Parent.AccessibleAt(declaration)
+            .Select(MapLookup);
 }
