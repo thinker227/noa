c9021f3aa1ba3fe34648a2d9fd9c32758e0e2a45
thinker227 <thinker0227@gmail.com>
2024-02-08 22:19:10 +0100

    Update SymbolResolutionTests

--- src/compiler-tests/Symbols/SymbolResolutionTests.cs
+++ src/compiler-tests/Symbols/SymbolResolutionTests.cs
@@ -10,6 +10,7 @@ public class SymbolResolutionTests
         var text = """
         let x = 0;
         let x = 1;
+        {}
         """;
         var source = new Source(text, "test-input");
         var ast = Ast.Create(source);
@@ -20,7 +21,8 @@ public class SymbolResolutionTests
 
         var x1Decl = (LetDeclaration)ast.Root.Statements[0].Declaration!;
         var x2Decl = (LetDeclaration)ast.Root.Statements[1].Declaration!;
-
+        var end = ast.Root.Statements[2];
+        
         var x1 = x1Decl.Symbol.Value;
         var x2 = x2Decl.Symbol.Value;
 
@@ -33,8 +35,68 @@ public class SymbolResolutionTests
         x2Lookup.Symbol.ShouldBe(x1);
         x2Lookup.Accessibility.ShouldBe(SymbolAccessibility.Accessible);
 
-        var endLookup = scope.LookupSymbol("x", null).ShouldNotBeNull();
+        var endLookup = scope.LookupSymbol("x", end).ShouldNotBeNull();
         endLookup.Symbol.ShouldBe(x2);
         endLookup.Accessibility.ShouldBe(SymbolAccessibility.Accessible);
+    }
+
+    [Fact]
+    public void EarlyLookup_Returns_DeclaredLater()
+    {
+        var text = """
+        let x = 0;
+        """;
+        var source = new Source(text, "test-input");
+        var ast = Ast.Create(source);
+
+        var diagnostics = SymbolResolution.ResolveSymbols(ast);
+
+        diagnostics.ShouldBeEmpty();
+
+        var decl = (LetDeclaration)ast.Root.Statements[0].Declaration!;
+        var x = decl.Symbol.Value;
+
+        var scope = decl.Scope.Value;
+
+        var lookup = scope.LookupSymbol("x", decl).ShouldNotBeNull();
+        lookup.Symbol.ShouldBe(x);
+        lookup.Accessibility.ShouldBe(SymbolAccessibility.DeclaredLater);
+    }
+
+    [Fact]
+    public void EndLookup_Returns_AllVariables()
+    {
+        var text = """
+        let x = 0;
+        let y = 0;
+        """;
+        var source = new Source(text, "test-input");
+        var ast = Ast.Create(source);
+
+        var diagnostics = SymbolResolution.ResolveSymbols(ast);
+
+        diagnostics.ShouldBeEmpty();
+
+        var x = ((LetDeclaration)ast.Root.Statements[0].Declaration!).Symbol.Value;
+        var y = ((LetDeclaration)ast.Root.Statements[1].Declaration!).Symbol.Value;
+
+        var scope = ast.Root.Statements[0].Scope.Value;
+
+        var xLookup = scope.LookupSymbol("x", null).ShouldNotBeNull();
+        xLookup.Symbol.ShouldBe(x);
+        xLookup.Accessibility.ShouldBe(SymbolAccessibility.Accessible);
+        
+        var yLookup = scope.LookupSymbol("y", null).ShouldNotBeNull();
+        yLookup.Symbol.ShouldBe(y);
+        yLookup.Accessibility.ShouldBe(SymbolAccessibility.Accessible);
+
+        var declared = scope.DeclaredAt(null);
+        declared.ShouldBe([x, y]);
+        
+        var accessible = scope.AccessibleAt(null);
+        accessible.ShouldBe([
+            new(x, SymbolAccessibility.Accessible),
+            new(y, SymbolAccessibility.Accessible)
+        ]);
     }
 }
