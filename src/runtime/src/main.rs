b0897f71c3f1482eb3776173470007738692c5b4
thinker227 <thinker0227@gmail.com>
2024-04-27 16:12:09 +0200

    Add a basic runtime CLI

--- src/runtime/src/main.rs
+++ src/runtime/src/main.rs
@@ -1,3 +1,28 @@
+use std::{fs, io, path::Path};
+
+use clap::Parser;
+use cli::Args;
+
+mod cli;
+mod runtime;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let bytecode = if let Some(path) = &args.bytecode_file_path {
+        read_bytecode_from_file_path(path)
+    } else {
+        todo!("Handle file path not being provided.")
+    };
+
+    println!("{:?}", bytecode);
+}
+
+#[derive(Debug)]
+enum BytecodeReadError {
+    IoError(io::Error)
+}
+
+fn read_bytecode_from_file_path(path: &Path) -> Result<Vec<u8>, BytecodeReadError> {
+    fs::read(path).map_err(|e| BytecodeReadError::IoError(e))
 }
