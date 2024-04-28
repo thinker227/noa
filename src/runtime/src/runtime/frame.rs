4fb0e48657b7829527b4048db8297afe4cfdeaa0
thinker227 <thinker0227@gmail.com>
2024-04-28 21:41:57 +0200

    Add frame

---
+++ src/runtime/src/runtime/frame.rs
@@ -0,0 +1,41 @@
+use super::{function::Function, opcode::Opcode};
+
+/// A frame which represents the state of the runtime
+/// within a single invocation of a function.
+#[derive(Debug)]
+pub struct StackFrame<'a> {
+    function: &'a Function,
+    ip: usize,
+}
+
+impl<'a> StackFrame<'a> {
+    /// Creates a new stack frame.
+    pub fn new(function: &'a Function) -> Self {
+        Self {
+            function,
+            ip: 0
+        }
+    }
+
+    /// Returns the function the stack frame represents an invocation of.
+    pub fn function(&self) -> &Function {
+        self.function
+    }
+
+    /// Returns the current op-code, if the stack frame hasn't finished.
+    pub fn current(&self) -> Option<Opcode> {
+        self.function.code().get(self.ip).copied()
+    }
+
+    /// Progresses to the next op-code, if the stack frame hasn't finished.
+    pub fn progress(&mut self) -> Option<Opcode> {
+        let opcode = self.current()?;
+        self.ip += 1;
+        Some(opcode)
+    }
+
+    /// Returns whether the stack frame has finished or not.
+    pub fn is_finished(&self) -> bool {
+        self.ip < self.function.code().len()
+    }
+}
