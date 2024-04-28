3b27374160adeef388a0996e5b01ab317781980e
thinker227 <thinker0227@gmail.com>
2024-04-29 01:11:46 +0200

    Add ark/header tests

--- src/runtime/src/ark.rs
+++ src/runtime/src/ark.rs
@@ -26,7 +26,7 @@ impl Ark {
         let (header_bytes, bytes) = split_const::<HEADER_SIZE>(bytes)
             .ok_or(ArkError::MissingHeader)?;
 
-        let header = Header::from_bytes(bytes)
+        let header = Header::from_bytes(header_bytes)
             .map_err(|e| ArkError::HeaderError(e))?;
 
         let (function_section, rest) = FunctionSection::from_bytes(bytes)
@@ -87,5 +87,134 @@ impl Header {
             main
         };
         Ok(header)
+    }
+}
+
+#[cfg(test)]
+mod ark_tests {
+    use super::*;
+
+    #[test]
+    fn reads_ark_file() {
+        let bytes = &[
+            116, 111, 116, 104, 101, 97, 114, 107,
+            0xe, 6, 2, 1,
+            0, 0, 0, 0
+        ];
+
+        let ark = Ark::from_bytes(bytes).unwrap();
+
+        matches!(ark, Ark {
+            header: Header {
+                identifier: IDENTIFIER,
+                main: 0x0e060201
+            },
+            function_section: FunctionSection {
+                functions_length: 0,
+                ..
+            }
+        });
+    }
+
+    #[test]
+    fn header_error() {
+        let bytes = &[
+            116, 111, 116, 104, 101, 97, 114, 107,
+            6, 9
+        ];
+
+        let e = Ark::from_bytes(bytes).unwrap_err();
+
+        matches!(e, ArkError::HeaderError(..));
+    }
+
+    #[test]
+    fn function_section_error() {
+        let bytes = &[
+            116, 111, 116, 104, 101, 97, 114, 107,
+            0xe, 6, 2, 1,
+            6, 9
+        ];
+
+        let e = Ark::from_bytes(bytes).unwrap_err();
+
+        matches!(e, ArkError::FunctionSectionError(..));
+    }
+
+    #[test]
+    fn too_many_bytes() {
+        let bytes = &[
+            116, 111, 116, 104, 101, 97, 114, 107,
+            0xe, 6, 2, 1,
+            0, 0, 0, 0,
+            0
+        ];
+
+        let e = Ark::from_bytes(bytes).unwrap_err();
+
+        matches!(e, ArkError::TooManyBytes);
+    }
+}
+
+#[cfg(test)]
+mod header_tests {
+    use super::*;
+
+    #[test]
+    fn reads_header() {
+        let bytes = &[
+            116, 111, 116, 104, 101, 97, 114, 107,
+            0xe, 6, 2, 1
+        ];
+
+        let header = Header::from_bytes(bytes).unwrap();
+
+        matches!(header, Header {
+            identifier: self::IDENTIFIER,
+            main: 0x0e060201
+        });
+    }
+
+    #[test]
+    fn missing_identifier() {
+        let bytes = &[0xb, 0xe, 0xe, 0xf];
+
+        let e = Header::from_bytes(bytes).unwrap_err();
+
+        assert_eq!(e, HeaderError::MissingIdentifier);
+    }
+
+    #[test]
+    fn bad_identifier() {
+        let bytes = &[0xb, 0xa, 0xd, 0xf, 0x0, 0x0, 0xd, 0x0];
+
+        let e = Header::from_bytes(bytes).unwrap_err();
+
+        assert_eq!(e, HeaderError::BadIdentifier);
+    }
+
+    #[test]
+    fn missing_main() {
+        let bytes = &[
+            116, 111, 116, 104, 101, 97, 114, 107,
+            6, 9
+        ];
+
+        let e = Header::from_bytes(bytes).unwrap_err();
+
+        assert_eq!(e, HeaderError::MissingMain);
+    }
+
+    #[test]
+    fn too_many_bytes() {
+        let bytes = &[
+            116, 111, 116, 104, 101, 97, 114, 107,
+            0xe, 6, 2, 1,
+            0
+        ];
+
+        let e = Header::from_bytes(bytes).unwrap_err();
+
+        assert_eq!(e, HeaderError::TooManyBytes);
     }
 }
