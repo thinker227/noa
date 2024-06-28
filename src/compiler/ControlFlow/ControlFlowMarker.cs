4a79e57937058dbcef1836f9419d01ef51750de1
thinker227 <thinker0227@gmail.com>
2024-06-28 11:10:15 +0200

    Fix some bugs

--- src/compiler/ControlFlow/ControlFlowMarker.cs
+++ src/compiler/ControlFlow/ControlFlowMarker.cs
@@ -93,8 +93,12 @@ file sealed class Visitor(Reachability current, CancellationToken cancellationTo
     protected override ControlFlowResult VisitReturnExpression(ReturnExpression node)
     {
         Visit(node.Expression);
-        
-        var next = current with { CanFallThrough = false, CanReturn = true };
+
+        var next = current with
+        {
+            CanFallThrough = false,
+            CanReturn = current.CanReturn || current.IsReachable
+        };
         return new(current, next);
     }
 
@@ -102,13 +106,21 @@ file sealed class Visitor(Reachability current, CancellationToken cancellationTo
     {
         Visit(node.Expression);
 
-        var next = current with { CanFallThrough = false, CanBreak = true };
+        var next = current with
+        {
+            CanFallThrough = false,
+            CanBreak = current.CanBreak || current.IsReachable
+        };
         return new(current, next);
     }
 
     protected override ControlFlowResult VisitContinueExpression(ContinueExpression node)
     {
-        var next = current with { CanFallThrough = false, CanContinue = true };
+        var next = current with
+        {
+            CanFallThrough = false,
+            CanContinue = current.CanContinue || current.IsReachable
+        };
         return new(current, next);
     }
 
@@ -127,8 +139,26 @@ file sealed class Visitor(Reachability current, CancellationToken cancellationTo
     protected override ControlFlowResult VisitLoopExpression(LoopExpression node)
     {
         var blockVisitor = CreateSubVisitor();
-        var next = blockVisitor.Visit(node.Block).Next;
-        
+        var tail = blockVisitor.Visit(node.Block).Next;
+
+        var next = tail switch
+        {
+            // The code can return and might be able to fall through using a break.
+            (_, true, var @break, _) =>
+                new(@break, true, false, false),
+            
+            // The code can fall through using a break.
+            (_, false, true, _) =>
+                Reachability.Reachable,
+            
+            // The code won't be able to fall through.
+            (true, false, false, _) or 
+            (_, false, false, true) =>
+                Reachability.Unreachable,
+            
+            (false, false, false, false) => Reachability.Unreachable
+        };
+
         return new(current, next);
     }
 
