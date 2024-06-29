1a81ef708ab0f6132b7355121b2d0670f0c208b8
thinker227 <thinker0227@gmail.com>
2024-06-29 22:42:08 +0200

    Add find references support

---
+++ src/lang-server/Features/FindReferences.cs
@@ -0,0 +1,45 @@
+using Draco.Lsp.Model;
+using Draco.Lsp.Server.Language;
+using Noa.Compiler.Nodes;
+
+namespace Noa.LangServer;
+
+public sealed partial class NoaLanguageServer : IFindReferences
+{
+    public Task<IList<Location>> FindReferencesAsync(ReferenceParams param, CancellationToken cancellationToken)
+    {
+        var documentUri = param.TextDocument.Uri;
+        
+        logger.Debug(
+            "Fetching all references for {documentUri} at position {line}:{character}",
+            documentUri,
+            param.Position.Line,
+            param.Position.Character);
+        
+        var document = GetOrCreateDocument(documentUri, cancellationToken);
+        var position = ToAbsolutePosition(param.Position, document.LineMap);
+        var node = document.Ast.Root.FindNodeAt(position);
+        
+        var symbol = node switch
+        {
+            Identifier { Parent.Value: FunctionDeclaration x } => x.Symbol.Value,
+            Identifier { Parent.Value: LetDeclaration x } => x.Symbol.Value,
+            Identifier { Parent.Value: Parameter x } => x.Symbol.Value,
+            IdentifierExpression x => x.ReferencedSymbol.Value,
+            _ => null
+        };
+        if (symbol is null) return Task.FromResult<IList<Location>>([]);
+
+        var references = document.GetReferences(symbol, param.Context.IncludeDeclaration);
+        var locations = references
+            .Select(x => ToLspLocation(x.Location, document))
+            .ToList();
+
+        return Task.FromResult<IList<Location>>(locations);
+    }
+
+    public ReferenceRegistrationOptions FindReferencesRegistrationOptions => new()
+    {
+        DocumentSelector = DocumentSelector
+    };
+}
--- src/lang-server/NoaDocument.cs
+++ src/lang-server/NoaDocument.cs
@@ -1,6 +1,30 @@
 using Draco.Lsp.Model;
 using Noa.Compiler;
+using Noa.Compiler.Nodes;
+using Noa.Compiler.Symbols;
 
 namespace Noa.LangServer;
 
-public readonly record struct NoaDocument(Ast Ast, LineMap LineMap, DocumentUri Uri);
+public sealed record NoaDocument(Ast Ast, LineMap LineMap, DocumentUri Uri)
+{
+    private Dictionary<ISymbol, IReadOnlyCollection<Node>>? references = null;
+
+    public IReadOnlyCollection<Node> GetReferences(ISymbol symbol, bool includeDeclaration)
+    {
+        references ??= [];
+
+        if (references.TryGetValue(symbol, out var cached)) return cached;
+
+        var nodes = new List<Node>();
+        if (includeDeclaration && symbol is IDeclaredSymbol declared) nodes.Add(declared.Declaration);
+
+        var referenceNodes = Ast.Root.DescendantsAndSelf()
+            .OfType<IdentifierExpression>()
+            .Where(x => x.ReferencedSymbol.Value == symbol);
+        nodes.AddRange(referenceNodes);
+
+        references.Add(symbol, nodes);
+        
+        return nodes;
+    }
+}
