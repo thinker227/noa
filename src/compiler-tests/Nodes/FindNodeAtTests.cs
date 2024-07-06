5ebb7dd4427baf631331131789ba9d2a7ed8ca60
thinker227 <thinker0227@gmail.com>
2024-07-06 21:12:19 +0200

    Update tests

--- src/compiler-tests/Nodes/FindNodeAtTests.cs
+++ src/compiler-tests/Nodes/FindNodeAtTests.cs
@@ -2,51 +2,52 @@ namespace Noa.Compiler.Nodes.Tests;
 
 public class FindNodeAtTests
 {
-    [Fact]
-    public void FindNodeAt_FindsNode_WithinChildren()
+    private static Node? Run(string text, int position)
     {
-        var text = """
-        let x = 12 + 34 * 56;
-        """;
-        //           ^
-        var position = 13;
         var source = new Source(text, "test-input");
         var ast = Ast.Parse(source);
 
         var node = ast.Root.FindNodeAt(position);
+        return node;
+    }
+    
+    private static T Run<T>(string text, int position) where T : Node
+    {
+        var node = Run(text, position);
+        return node.ShouldBeOfType<T>();
+    }
+    
+    [Fact]
+    public void FindNodeAt_FindsNode_WithinChildren()
+    {
+        var node = Run<NumberExpression>(
+            """
+            let x = 12 + 34 * 56;
+            """,
+            //           ^
+            13);
 
-        var num = node.ShouldBeOfType<NumberExpression>();
-        num.Value.ShouldBe(34);
+        node.Value.ShouldBe(34);
     }
 
     [Fact]
     public void FindNodeAt_ReturnsNull_ForPositionOutsideNode()
     {
-        var text = """
-        let x = 0;
-        """;
-        var position = 20;
-        var source = new Source(text, "test-input");
-        var ast = Ast.Parse(source);
-
-        var node = ast.Root.FindNodeAt(position);
+        var node = Run(
+            """
+            let x = 0;
+            """,
+            20);
 
         node.ShouldBeNull();
     }
 
     [Fact]
-    public void FindNodeAt_ReturnsContainingNode_ForPositionInWhitespace()
-    {
-        var text = """
-        let x = 0;
-        """;
-        //   ^
-        var position = 5;
-        var source = new Source(text, "test-input");
-        var ast = Ast.Parse(source);
-
-        var node = ast.Root.FindNodeAt(position);
-
-        node.ShouldBeOfType<LetDeclaration>();
-    }
+    public void FindNodeAt_ReturnsContainingNode_ForPositionInWhitespace() =>
+        Run<LetDeclaration>(
+            """
+            let x = 0;
+            """,
+            //   ^
+            5);
 }
