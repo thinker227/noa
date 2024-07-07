2ea3b05a55ca7e5f6d42abae92563b7ef674fb01
thinker227 <thinker0227@gmail.com>
2024-07-07 23:19:32 +0200

    Fix up templates and nodes

--- src/compiler/Nodes/Nodes.cs
+++ src/compiler/Nodes/Nodes.cs
@@ -44,103 +44,39 @@ public abstract class Node
             : [];
 }
 
-public sealed class Root : BlockExpression
+public sealed partial class Root
 {
     public Semantic<TopLevelFunction> Function { get; internal set; }
 }
 
-public sealed class Identifier : Node
-{
-    public required string Name { get; init; }
-
-    public override IEnumerable<Node> Children => [];
-}
-
-public abstract class Statement : Node
+public abstract partial class Statement
 {
     public Semantic<Reachability> Reachability { get; internal set; }
 }
 
-public sealed class Parameter : Node
+public sealed partial class Parameter
 {
-    public required bool IsMutable { get; init; }
-    
-    public required Identifier Identifier { get; init; }
-
     public Semantic<ParameterSymbol> Symbol { get; internal set; }
-
-    public override IEnumerable<Node> Children => [Identifier];
 }
 
-public abstract class Declaration : Statement;
-
-public sealed class FunctionDeclaration : Declaration
+public sealed partial class FunctionDeclaration
 {
-    public required Identifier Identifier { get; init; }
-    
-    public required ImmutableArray<Parameter> Parameters { get; init; }
-    
-    public required Expression? ExpressionBody { get; init; }
-    
-    public required BlockExpression? BlockBody { get; init; }
-
     public Semantic<NomialFunction> Symbol { get; internal set; }
-
-    public override IEnumerable<Node> Children => [
-        Identifier,
-        ..Parameters,
-        ..EmptyIfNull(ExpressionBody),
-        ..EmptyIfNull(BlockBody)
-    ];
 }
 
-public sealed class LetDeclaration : Declaration
+public sealed partial class LetDeclaration
 {
-    public required bool IsMutable { get; init; }
-    
-    public required Identifier Identifier { get; init; }
-    
-    public required Expression Expression { get; init; }
 
     public Semantic<VariableSymbol> Symbol { get; internal set; }
-
-    public override IEnumerable<Node> Children => [Identifier, Expression];
 }
 
-public sealed class AssignmentStatement : Statement
-{
-    public required Expression Target { get; init; }
-    
-    public required Expression Value { get; init; }
-
-    public override IEnumerable<Node> Children => [Target, Value];
-}
-
-public sealed class ExpressionStatement : Statement
-{
-    public required Expression Expression { get; init; }
-
-    public override IEnumerable<Node> Children => [Expression];
-}
-
-public abstract class Expression : Node
+public abstract partial class Expression
 {
     public Semantic<Reachability> Reachability { get; internal set; }
 }
 
-public sealed class ErrorExpression : Expression
+public partial class BlockExpression
 {
-    public override IEnumerable<Node> Children => [];
-}
-
-public class BlockExpression : Expression
-{
-    public required ImmutableArray<Statement> Statements { get; init; }
-    
-    public required Expression? TrailingExpression { get; init; }
-
-    public override IEnumerable<Node> Children => [..Statements, ..EmptyIfNull(TrailingExpression)];
-    
     /// <summary>
     /// The scope <i>declared</i> by the scope,
     /// different from the scope the block is <i>in</i>.
@@ -153,132 +89,30 @@ public class BlockExpression : Expression
     public Semantic<Reachability> TailReachability { get; internal set; }
 }
 
-public sealed class CallExpression : Expression
+public sealed partial class LambdaExpression
 {
-    public required Expression Target { get; init; }
-    
-    public required ImmutableArray<Expression> Arguments { get; init; }
-
-    public override IEnumerable<Node> Children => [Target, ..Arguments];
-}
-
-public sealed class LambdaExpression : Expression
-{
-    public required ImmutableArray<Parameter> Parameters { get; init; }
-    
-    public required Expression Body { get; init; }
-
-    public override IEnumerable<Node> Children => [..Parameters, Body];
-    
     public Semantic<LambdaFunction> Function { get; internal set; }
 }
 
-public sealed class TupleExpression : Expression
+public sealed partial class ReturnExpression
 {
-    public required ImmutableArray<Expression> Expressions { get; init; }
-
-    public override IEnumerable<Node> Children => Expressions;
-}
-
-public sealed class IfExpression : Expression
-{
-    public required Expression Condition { get; init; }
-    
-    public required BlockExpression IfTrue { get; init; }
-    
-    public required BlockExpression IfFalse { get; init; }
-
-    public override IEnumerable<Node> Children => [Condition, IfTrue, IfFalse];
-}
-
-public sealed class LoopExpression : Expression
-{
-    public required BlockExpression Block { get; init; }
-
-    public override IEnumerable<Node> Children => [Block];
-}
-
-public sealed class ReturnExpression : Expression
-{
-    public required Expression? Expression { get; init; }
-
-    public override IEnumerable<Node> Children =>
-        Expression is not null
-            ? [Expression]
-            : [];
-    
     public Semantic<IFunction?> Function { get; internal set; }
 }
 
-public sealed class BreakExpression : Expression
+public sealed partial class BreakExpression
 {
-    public required Expression? Expression { get; init; }
-
-    public override IEnumerable<Node> Children =>
-        Expression is not null
-            ? [Expression]
-            : [];
-    
     public Semantic<LoopExpression?> Loop { get; internal set; }
 }
 
-public sealed class ContinueExpression : Expression
+public sealed partial class ContinueExpression
 {
-    public override IEnumerable<Node> Children => [];
-    
     public Semantic<LoopExpression?> Loop { get; internal set; }
 }
 
-public sealed class UnaryExpression : Expression
+public sealed partial class IdentifierExpression
 {
-    public required UnaryKind Kind { get; init; }
-    
-    public required Expression Operand { get; init; }
-
-    public override IEnumerable<Node> Children => [Operand];
-}
-
-public sealed class BinaryExpression : Expression
-{
-    public required Expression Left { get; init; }
-    
-    public required BinaryKind Kind { get; init; }
-    
-    public required Expression Right { get; init; }
-
-    public override IEnumerable<Node> Children => [Left, Right];
-}
-
-public sealed class IdentifierExpression : Expression
-{
-    public required string Identifier { get; init; }
-
     public Semantic<ISymbol> ReferencedSymbol { get; internal set; }
-
-    public override IEnumerable<Node> Children => [];
 }
-
-public sealed class StringExpression : Expression
-{
-    public required string Value { get; init; }
-
-    public override IEnumerable<Node> Children => [];
-}
-
-public sealed class BoolExpression : Expression
-{
-    public required bool Value { get; init; }
-
-    public override IEnumerable<Node> Children => [];
-}
-
-public sealed class NumberExpression : Expression
-{
-    public required int Value { get; init; }
-
-    public override IEnumerable<Node> Children => [];
-}
-
 public enum UnaryKind
 {
     Identity,
