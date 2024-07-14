1181045a60543b75a474bba354a0983852ca580e
thinker227 <thinker0227@gmail.com>
2024-07-15 00:57:20 +0200

    Display error when failing to restart language server

--- src/vscode-extension/src/commands.ts
+++ src/vscode-extension/src/commands.ts
@@ -1,5 +1,10 @@
+import { window } from "vscode";
 import { restartLanguageServer } from "./lang_server";
 
 export async function restartLangServer() {
-    await restartLanguageServer();
+    let success = await restartLanguageServer();
+
+    if (!success) {
+        window.showErrorMessage("Failed to restart the language server.");
+    }
 }
