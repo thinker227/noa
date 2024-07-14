2e931f7d1ca8b7d9155a86d42eefb535b8415c3e
thinker227 <thinker0227@gmail.com>
2024-07-15 00:56:15 +0200

    Add some additional config functions

--- src/vscode-extension/src/config.ts
+++ src/vscode-extension/src/config.ts
@@ -3,3 +3,11 @@ import { WorkspaceConfiguration, workspace } from "vscode";
 export function getConfig(): WorkspaceConfiguration {
     return workspace.getConfiguration("noa-lang");
 }
+
+export function getCliCommand(): string {
+    return getConfig().get("cliExecutable") || "noa";
+}
+
+export function getServerLogPath(): string | undefined {
+    return getConfig().get("serverLogPath");
+}
