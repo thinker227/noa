b27102e9e0da3cf51fab0211737b0a86d0436eca
Thinker <thinker0227@gmail.com>
2025-01-27 09:07:57 +0100

    Fix symbol references in language server

--- src/lang-server/Features/Rename.cs
+++ src/lang-server/Features/Rename.cs
@@ -50,13 +50,16 @@ public sealed partial class NoaLanguageServer : IRename, IPrepareRename
 
         if (symbol is not IDeclaredSymbol declared) return Task.FromResult<WorkspaceEdit?>(null);
 
-        var references = document.GetReferences(declared, true);
+        var references = document.GetReferences(declared);
 
-        var textEdits = references.Select(x => new TextEdit()
-        {
-            Range = ToLspRange(x.Span, document),
-            NewText = param.NewName
-        });
+        var textEdits = references
+            .Select(x => x.Span)
+            .Prepend(declared.DefinitionLocation.Span)
+            .Select(span => new TextEdit()
+            {
+                Range = ToLspRange(span, document),
+                NewText = param.NewName
+            });
         var textDocumentEdit = new TextDocumentEdit()
         {
             TextDocument = new() { Uri = documentUri, Version = null },
