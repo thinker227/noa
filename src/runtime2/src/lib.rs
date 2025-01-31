ccebd2018e4ada6a49f40e512de7ef61d0acc6c8
Thinker <thinker0227@gmail.com>
2025-01-31 16:06:36 +0100

    Add basic value representation

--- src/runtime2/src/lib.rs
+++ src/runtime2/src/lib.rs
@@ -1,2 +1,5 @@
+#![feature(never_type)]
+
 pub mod opcode;
 pub mod ark;
+pub mod value;
---
+++ src/runtime2/src/value.rs
@@ -0,0 +1,78 @@
+use crate::ark::FuncId;
+
+/// The type of a runtime value.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Type {
+    Number,
+    Bool,
+    Function,
+    String,
+    List,
+    Nil
+}
+
+/// A runtime value.
+#[derive(Debug)]
+pub enum Value {
+    /// A number.
+    Number(f64),
+    /// A boolean.
+    Bool(bool),
+    /// A function.
+    Function(FuncId), // todo, should store a closure
+    /// A heap-allocated object.
+    Object(!), // todo
+    /// `()`
+    Nil
+}
+
+impl From<f64> for Value {
+    fn from(value: f64) -> Self {
+        Self::Number(value)
+    }
+}
+
+impl From<bool> for Value {
+    fn from(value: bool) -> Self {
+        Self::Bool(value)
+    }
+}
+
+impl From<()> for Value {
+    fn from(_: ()) -> Self {
+        Self::Nil
+    }
+}
+
+impl TryFrom<&Value> for f64 {
+    type Error = ();
+
+    fn try_from(value: &Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Number(n) => Ok(*n),
+            _ => Err(())
+        }
+    }
+}
+
+impl TryFrom<&Value> for bool {
+    type Error = ();
+
+    fn try_from(value: &Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Bool(x) => Ok(*x),
+            _ => Err(())
+        }
+    }
+}
+
+impl TryFrom<&Value> for () {
+    type Error = ();
+
+    fn try_from(value: &Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Nil => Ok(()),
+            _ => Err(())
+        }
+    }
+}
