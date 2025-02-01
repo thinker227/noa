51d9f2cc92860a9b31b924e69f4ad518695afb02
Thinker <thinker0227@gmail.com>
2025-02-01 20:35:57 +0100

    Update main

--- src/runtime2_cli/src/main.rs
+++ src/runtime2_cli/src/main.rs
@@ -6,7 +6,9 @@ use args::Args;
 use exit::{Exit, IntoExit};
 use binrw::BinRead;
 use clap::Parser;
-use noa_runtime::ark::Ark;
+
+use noa_runtime::vm::Vm;
+use noa_runtime::ark::{Ark, CodeSection, FunctionSection, StringSection};
 
 mod args;
 mod exit;
@@ -14,11 +16,29 @@ mod exit;
 fn main() -> Exit<()> {
     let args = Args::try_parse().into_exit()?;
 
-    let _ark = {
+    let ark = {
         let bytes = fs::read(args.ark_file_path).into_exit()?;
         let mut cursor = Cursor::new(bytes);
         Ark::read_be(&mut cursor).into_exit()?
     };
+
+    let Ark {
+        function_section: FunctionSection {
+            functions,
+            ..
+        },
+        code_section: CodeSection {
+            code,
+            ..
+        },
+        string_section: StringSection {
+            strings,
+            ..
+        },
+        ..
+    } = ark;
+
+    let _vm = Vm::new(functions, strings, code, 100_000);
 
     Exit::ok()
 }
