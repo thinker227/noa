53ec1b30accb05b12d35ceb685b7630f48cdd3b4
Thinker <thinker0227@gmail.com>
2025-02-06 00:19:54 +0100

    Add updateInteractively

--- src/vscode-extension/src/config.ts
+++ src/vscode-extension/src/config.ts
@@ -1,4 +1,4 @@
-import { WorkspaceConfiguration, workspace } from "vscode";
+import { ConfigurationTarget, WorkspaceConfiguration, window, workspace } from "vscode";
 
 /**
  * Gets the extension configuration.
@@ -26,4 +26,55 @@ export function getServerLogPath(): string | undefined {
  */
 export function getLogLevel(): string {
     return getConfig().get("logLevel") || "info";
+}
+
+/**
+ * Interactively updates a configuration section.
+ * @param section The the config section to update.
+ * @param title The title input box.
+ * @param prompt The input box prompt.
+ * @returns The new value of the configuration, or undefined if canceled.
+ */
+export async function updateInteractively(
+    section: string,
+    title?: string,
+    prompt?: string
+): Promise<string | undefined> {
+    let value = await window.showInputBox({
+        title,
+        prompt
+    });
+
+    if (!value) {
+        return undefined;
+    }
+
+    let target = await window.showQuickPick([
+        {
+            label: "Global",
+            description: "Set the configuration globally",
+            target: ConfigurationTarget.Global
+        },
+        {
+            label: "Workspace",
+            description: "Set the configuration in the workspace",
+            target: ConfigurationTarget.Workspace
+        },
+        {
+            label: "Workspace folder",
+            description: "Set the configuration in the workspace folder",
+            target: ConfigurationTarget.WorkspaceFolder
+        }
+    ], {
+        title: "Where should the configuration be set?",
+        canPickMany: false,
+    });
+
+    if (!target) {
+        return undefined;
+    }
+
+    await getConfig().update(section, value, target.target);
+
+    return value;
 }
