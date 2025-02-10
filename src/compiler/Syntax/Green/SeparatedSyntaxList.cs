f15d8d39efdba020c884bfdbc8b17d053041529d
Thinker <thinker0227@gmail.com>
2025-02-11 00:42:32 +0100

    Make SeparatedSyntaxLists into classes

--- src/compiler/Syntax/Green/SeparatedSyntaxList.cs
+++ src/compiler/Syntax/Green/SeparatedSyntaxList.cs
@@ -1,43 +1,81 @@
 using System.Collections;
+using System.Reflection;
 
 namespace Noa.Compiler.Syntax.Green;
 
-internal sealed class SeparatedSyntaxList<TNode> : IReadOnlyList<NodeOrToken<TNode>>
+internal sealed class SeparatedSyntaxList<TNode> : SyntaxNode, IReadOnlyList<SyntaxNode>
     where TNode : SyntaxNode
 {
-    public ImmutableArray<TNode> Nodes { get; }
+    // Hack because C# doesn't have associated types.
+    // Get a reference to the ConstructorInfo for the (Green.SyntaxNode, int, Syntax.SyntaxNode, IReadOnlyList<Green.SyntaxNode>)
+    // constructor of Syntax.SeparatedSyntaxList<TRed> so we can instantiate it without statically knowing
+    // the type of TRed.
 
-    public ImmutableArray<Token> Tokens { get; }
+    private static Type RedElementType { get; } = Assembly
+        .GetExecutingAssembly()
+        .GetType($"Noa.Compiler.Syntax.{typeof(TNode).Name}")!;
+    
+    private static Type RedNodeType { get; } = typeof(Syntax.SeparatedSyntaxList<>).MakeGenericType(RedElementType);
 
-    public int Count => Nodes.Length + Tokens.Length;
+    private static ConstructorInfo RedNodeConstructor { get; } = RedNodeType.GetConstructor(
+        BindingFlags.NonPublic | BindingFlags.Instance,
+        [
+            typeof(Green.SyntaxNode),
+            typeof(int),
+            typeof(Syntax.SyntaxNode),
+            typeof(IReadOnlyList<Green.SyntaxNode>)
+        ])!;
+    
+    private readonly ImmutableArray<SyntaxNode> elements;
 
+    public int Count => elements.Length;
 
-    public NodeOrToken<TNode> this[int index] => index % 2 == 0
-        ? Nodes[index]
-        : Tokens[index];
+    public SyntaxNode this[int index] => elements[index];
 
-    public SeparatedSyntaxList(ImmutableArray<TNode> nodes, ImmutableArray<Token> tokens)
+    private SeparatedSyntaxList(ImmutableArray<SyntaxNode> elements) =>
+        this.elements = elements;
+
+    public static SeparatedSyntaxList<TNode> Create(IEnumerable<TNode> nodes, IEnumerable<Token> tokens)
     {
-        if (tokens.Length != nodes.Length && tokens.Length != nodes.Length - 1)
-            throw new ArgumentOutOfRangeException(nameof(tokens),
-                "Amount of tokens have to be equal to the amount of nodes or 1 less.");
-        
-        Nodes = nodes;
-        Tokens = tokens;
-    }
+        var ne = nodes.GetEnumerator();
+        var te = tokens.GetEnumerator();
 
-    public int GetWidth() =>
-        Nodes.Sum(x => x.GetWidth()) + Tokens.Sum(x => x.GetWidth());
+        var elements = ImmutableArray.CreateBuilder<SyntaxNode>();
 
-    public IEnumerator<NodeOrToken<TNode>> GetEnumerator()
-    {
-        for (var i = 0; i < Nodes.Length; i++)
+        while (ne.MoveNext())
         {
-            yield return Nodes[i];
+            elements.Add(ne.Current);
 
-            if (i < Tokens.Length) yield return Tokens[i];
+            if (!te.MoveNext())
+            {
+                if (ne.MoveNext()) throw new ArgumentException(
+                    "Amount of tokens has to be equal to the amount of nodes or 1 less.",
+                    nameof(tokens));
+
+                break;
+            }
+            else elements.Add(te.Current);
         }
+
+        if (te.MoveNext())
+        {
+            elements.Add(te.Current);
+
+            if (te.MoveNext()) throw new ArgumentException(
+                "Amount of tokens has to be equal to the amount of nodes or 1 less.",
+                nameof(tokens));
+        }
+
+        return new(elements.ToImmutable());
     }
+
+    public override int GetWidth() =>
+        elements.Sum(x => x.GetWidth());
+
+    public IEnumerator<SyntaxNode> GetEnumerator() => elements.AsEnumerable().GetEnumerator();
 
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public override Syntax.SyntaxNode ToRed(int position, Syntax.SyntaxNode parent) =>
+        (Syntax.SyntaxNode)RedNodeConstructor.Invoke([this, position, parent, elements]);
 }
--- src/compiler/Syntax/SeparatedSyntaxList.cs
+++ src/compiler/Syntax/SeparatedSyntaxList.cs
@@ -2,29 +2,93 @@ using System.Collections;
 
 namespace Noa.Compiler.Syntax;
 
-public readonly struct SeparatedSyntaxList<T> : IReadOnlyList<NodeOrToken<T>> where T : SyntaxNode
+public sealed class SeparatedSyntaxList<TNode> : SyntaxNode, IReadOnlyList<SyntaxNode>
+    where TNode : SyntaxNode
 {
-    private readonly int position;
-    private readonly SyntaxNode parent;
-    private readonly IReadOnlyList<Green.SyntaxNode> nodes;
-    private readonly IReadOnlyList<Green.Token> tokens;
+    private readonly SyntaxNode?[] constructed;
+    private readonly IReadOnlyList<Green.SyntaxNode> elements;
 
-    public NodeOrToken<T> this[int index] => throw new NotImplementedException();
 
-    public int Count => throw new NotImplementedException();
+    public SyntaxNode this[int index] => GetElemAt(index);
+
+    public int Count => elements.Count;
+
+    public int NodesCount => Count / 2 + 1;
+
+    public int TokensCount => Count / 2;
 
     internal SeparatedSyntaxList(
+        Green.SyntaxNode green,
         int position,
-        SyntaxNode parent,
-        IReadOnlyList<Green.SyntaxNode> nodes,
-        IReadOnlyList<Green.Token> tokens)
+        Syntax.SyntaxNode parent,
+        IReadOnlyList<Green.SyntaxNode> elements)
+        : base(green, position, parent)
     {
-        this.position = position;
-        this.parent = parent;
-        this.nodes = nodes;
-        this.tokens = tokens;
+        constructed = new SyntaxNode[elements.Count];
+        this.elements = elements;
     }
 
-    public IEnumerator<NodeOrToken<T>> GetEnumerator() => throw new NotImplementedException();
+    public IEnumerable<TNode> Nodes() =>
+        this.OfType<TNode>();
+    
+    public IEnumerable<Token> Tokens() =>
+        this.OfType<Token>();
+
+    public TNode GetNodeAt(int index)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(index, 0);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, NodesCount);
+        return (TNode)GetElemAt(index * 2);
+    }
+
+    public TNode GetTokenAt(int index)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(index, 0);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(index, TokensCount);
+        return (TNode)GetElemAt(index * 2 + 1);
+    }
+
+    private SyntaxNode GetElemAt(int index)
+    {
+        if (constructed[index] is { } x) return x;
+
+        var offset = GetOffsetAt(index);
+
+        var elem = elements[index].ToRed(position + offset, this);
+        constructed[index] = elem;
+
+        return elem;
+    }
+
+    private int GetOffsetAt(int index)
+    {
+        var offset = 0;
+
+        for (var i = 0; i < index; i++)
+            offset += elements[i].GetWidth();
+
+        return offset;
+    }
+
+    public IEnumerator<SyntaxNode> GetEnumerator()
+    {
+        var offset = 0;
+
+        for (var i = 0; i < Count; i++)
+        {
+            SyntaxNode elem;
+            if (constructed[i] is { } x) elem = x;
+            else
+            {
+                elem = elements[i].ToRed(position + offset, this);
+                constructed[i] = elem;
+            }
+
+            offset += elem.Span.Length;
+
+            yield return elem;
+        }
+    }
+
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
