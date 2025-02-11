b8c32a8f89bcaf8de215fde48a83dc6630bf2802
Thinker <thinker0227@gmail.com>
2025-02-11 21:47:21 +0100

    Add some docs to green SyntaxNode

--- src/compiler/Syntax/Green/SyntaxNode.cs
+++ src/compiler/Syntax/Green/SyntaxNode.cs
@@ -2,15 +2,29 @@ using System.Runtime.CompilerServices;
 
 namespace Noa.Compiler.Syntax.Green;
 
+/// <summary>
+/// An internal "green" concrete syntax node.
+/// <br/><br/>
+/// Red nodes (<see cref="Syntax.SyntaxNode"/>) are public API wrappers around green nodes.
+/// Red nodes have absolute spans and back-references to their parents, while green nodes don't.
+/// Green nodes are also meant to be incrementally reusable.
+/// </summary>
 internal abstract class SyntaxNode
 {
     private static readonly ConditionalWeakTable<SyntaxNode, List<PartialDiagnostic>> diagnostics = [];
 
+    /// <summary>
+    /// The diagnostics for the node.
+    /// </summary>
     public IReadOnlyCollection<PartialDiagnostic> Diagnostics =>
         diagnostics.TryGetValue(this, out var ds)
             ? ds
             : [];
     
+    /// <summary>
+    /// Adds a diagnostic to the node.
+    /// </summary>
+    /// <param name="diagnostic">The partial diagnostic to add.</param>
     public void AddDiagnostic(PartialDiagnostic diagnostic)
     {
         if (!diagnostics.TryGetValue(this, out var ds))
@@ -22,7 +36,16 @@ internal abstract class SyntaxNode
         ds.Add(diagnostic);
     }
 
+    /// <summary>
+    /// Gets the width of the node, including trivia.
+    /// </summary>
+    /// <returns></returns>
     public abstract int GetWidth();
 
+    /// <summary>
+    /// Creates a red node from this green node.
+    /// </summary>
+    /// <param name="position">The position of the red node.</param>
+    /// <param name="parent">The parent of the red node.</param>
     public abstract Syntax.SyntaxNode ToRed(int position, Syntax.SyntaxNode parent);
 }
