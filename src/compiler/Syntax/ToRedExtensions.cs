e2cd92320f4e27ed8215d007189d6f268f0cd539
Thinker <thinker0227@gmail.com>
2025-02-11 01:16:24 +0100

    Remove ToRedExtensions and WidthExtensions

--- src/compiler/Syntax/ToRedExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Noa.Compiler.Syntax;
-
-internal static class ToRedExtensions
-{
-    public static SyntaxList<TRed> ToRed<TGreen, TRed>(this ImmutableArray<TGreen> green, int position, SyntaxNode parent)
-        where TGreen : Green.SyntaxNode
-        where TRed : SyntaxNode =>
-        new(position, parent, green);
-}
--- src/compiler/Syntax/WidthExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace Noa.Compiler.Syntax;
-
-internal static class WidthExtensions
-{
-    public static int GetWidth<T>(this IEnumerable<T> xs) where T : Green.SyntaxNode =>
-        xs.Sum(x => x.GetWidth());
-}
