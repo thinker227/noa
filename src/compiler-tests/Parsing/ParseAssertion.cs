7accbf0bc10044dc13bf7e22f4328c04015559df
Thinker <thinker0227@gmail.com>
2025-02-14 23:12:32 +0100

    Better parse diagnostic checks

--- src/compiler-tests/Parsing/ParseAssertion.cs
+++ src/compiler-tests/Parsing/ParseAssertion.cs
@@ -7,12 +7,14 @@ namespace Noa.Compiler.Parsing.Tests;
 internal sealed class ParseAssertion
 {
     private readonly IEnumerator<SyntaxNode> nodes;
+    private Dictionary<DiagnosticId, IPartialDiagnostic> currentNodeDiagnostics;
 
     public Source Source { get; }
 
     private ParseAssertion(IEnumerator<SyntaxNode> nodes, Source source)
     {
         this.nodes = nodes;
+        currentNodeDiagnostics = [];
         Source = source;
     }
 
@@ -35,8 +37,26 @@ internal sealed class ParseAssertion
             .SelectMany(EnumerateNodes)
             .Prepend(root);
     
+    private void UpdateCurrentNodeDiagnostics(SyntaxNode node) =>
+        currentNodeDiagnostics = node.Diagnostics.ToDictionary(x => x.Template.Id);
+    
+    private void EnsureDiagnosticsChecked()
+    {
+        if (nodes.Current is not { } node) return;
+
+        if (currentNodeDiagnostics.Count != 0)
+        {
+            throw new Exception($"""
+            Current node ({node.GetType().Name}) had unchecked diagnostics.
+            Diagnostics: {string.Join(", ", currentNodeDiagnostics.Keys)}
+            """);
+        }
+    }
+    
     public Token E(Action<Token>? assert = null)
     {
+        EnsureDiagnosticsChecked();
+
         nodes.MoveNext().ShouldBeTrue();
 
         var token = nodes.Current.ShouldBeOfType<Token>();
@@ -46,33 +66,51 @@ internal sealed class ParseAssertion
         return token;
     }
 
-    public void D(params IEnumerable<DiagnosticId> ids)
+    public void D(DiagnosticId id)
     {
         var node = nodes.Current.ShouldNotBeNull();
-        node.Diagnostics.Select(x => x.Template.Id).ShouldBe(ids, ignoreOrder: true);
+
+        if (!currentNodeDiagnostics.Remove(id))
+        {
+            throw new Exception($"""
+            A diagnostic with the ID {id} was expected on the current node ({node.GetType().Name}) but none was found.
+            """);
+        }
     }
     
     public Token T(TokenKind kind, Action<Token>? assert = null)
     {
+        EnsureDiagnosticsChecked();
+
         nodes.MoveNext().ShouldBeTrue();
 
         var token = nodes.Current.ShouldBeOfType<Token>();
         token.Kind.ShouldBe(kind);
         assert?.Invoke(token);
 
+        UpdateCurrentNodeDiagnostics(token);
+
         return token;
     }
 
     public T N<T>(Action<T>? assert = null) where T : SyntaxNode
     {
+        EnsureDiagnosticsChecked();
+
         nodes.MoveNext().ShouldBeTrue();
 
         var node = nodes.Current.ShouldBeOfType<T>();
         assert?.Invoke(node);
 
+        UpdateCurrentNodeDiagnostics(node);
+
         return node;
     }
 
-    public void End() =>
+    public void End()
+    {
+        EnsureDiagnosticsChecked();
+
         nodes.MoveNext().ShouldBeFalse();
+    }
 }
