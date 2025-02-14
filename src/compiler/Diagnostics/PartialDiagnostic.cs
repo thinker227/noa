3eb1de8523ae823c44df7996b4af7aab9d04e21e
Thinker <thinker0227@gmail.com>
2025-02-14 17:06:37 +0100

    Add PartialDiagnostic

---
+++ src/compiler/Diagnostics/PartialDiagnostic.cs
@@ -0,0 +1,90 @@
+using TextMappingUtils;
+
+namespace Noa.Compiler.Diagnostics;
+
+/// <summary>
+/// A diagnostic with partial information about its location,
+/// filled in after the fact once exact location information is known.
+/// </summary>
+public interface IPartialDiagnostic
+{
+    /// <summary>
+    /// The template to create the diagnostic from.
+    /// </summary>
+    IDiagnosticTemplate Template { get; }
+
+    /// <summary>
+    /// The offset of the start of the diagnostic relative to the supplied location.
+    /// </summary>
+    int Offset { get; }
+
+    /// <summary>
+    /// The width of the diagnostic from its start.
+    /// </summary>
+    int Width { get; }
+
+    /// <summary>
+    /// Creates a full diagnostic by supplying a full position within the source to use as the location.
+    /// </summary>
+    /// <param name="source">The source the diagnostic is located in.</param>
+    /// <param name="fullPosition">The full position in the source text which the diagnostic is relative to.</param>
+    IDiagnostic Format(Source source, int fullPosition);
+}
+
+/// <summary>
+/// A diagnostic with partial information about its location,
+/// filled in after the fact once exact location information is known.
+/// </summary>
+/// <param name="Template">The template to create the diagnostic from.</param>
+/// <param name="Offset">The offset of the start of the diagnostic relative to the supplied location.</param>
+/// <param name="Width">The width of the diagnostic from its start.</param>
+public sealed record class PartialDiagnostic(
+    DiagnosticTemplate Template,
+    int Offset,
+    int Width)
+    : IPartialDiagnostic
+{
+    IDiagnosticTemplate IPartialDiagnostic.Template => Template;
+
+    public IDiagnostic Format(Source source, int fullPosition)
+    {
+        var span = new TextSpan(
+            start: fullPosition - Offset,
+            end: fullPosition - Offset + Width);
+        
+        return Diagnostic.Create(
+            Template,
+            new Location(source.Name, span));
+    }
+}
+
+/// <summary>
+/// A diagnostic with partial information about its location,
+/// filled in after the fact once exact location information is known.
+/// </summary>
+/// <typeparam name="TArg">The type of the argument to the template.</typeparam>
+/// <param name="Template">The template to create the diagnostic from.</param>
+/// <param name="Argument">The argument to supply to the template.</param>
+/// <param name="Offset">The offset of the start of the diagnostic relative to the supplied location.</param>
+/// <param name="Width">The width of the diagnostic from its start.</param>
+public sealed record class PartialDiagnostic<TArg>(
+    DiagnosticTemplate<TArg> Template,
+    TArg Argument,
+    int Offset,
+    int Width)
+    : IPartialDiagnostic
+{
+    IDiagnosticTemplate IPartialDiagnostic.Template => Template;
+
+    public IDiagnostic Format(Source source, int fullPosition)
+    {
+        var span = new TextSpan(
+            start: fullPosition - Offset,
+            end: fullPosition - Offset + Width);
+        
+        return Diagnostic.Create(
+            Template,
+            Argument,
+            new Location(source.Name, span));
+    }
+}
