93e4bc319ba3da6f705c6ae1c342fa1a1bcddef2
Thinker <thinker0227@gmail.com>
2025-02-15 14:48:04 +0100

    Implement IReadOnlyList for green SyntaxList

--- src/compiler/Syntax/Green/SyntaxList.cs
+++ src/compiler/Syntax/Green/SyntaxList.cs
@@ -1,13 +1,23 @@
 
+using System.Collections;
+
 namespace Noa.Compiler.Syntax.Green;
 
-internal sealed class SyntaxList<TNode>(ImmutableArray<TNode> nodes) : SyntaxNode
+internal sealed class SyntaxList<TNode>(ImmutableArray<TNode> nodes)
+    : SyntaxNode, IReadOnlyList<TNode>
     where TNode : SyntaxNode
 {
-    public override IEnumerable<SyntaxNode> Children => nodes;
+    public TNode this[int index] => nodes[index];
 
+    public int Count => nodes.Length;
+
+    public override IEnumerable<SyntaxNode> Children => nodes;
     public override int GetFullWidth() => nodes.Sum(x => x.GetFullWidth());
 
     public override Syntax.SyntaxNode ToRed(int position, Syntax.SyntaxNode parent) =>
         (Syntax.SyntaxNode)ReflectionInfo<TNode>.RedSyntaxListConstructor.Invoke([this, position, parent, nodes]);
+
+    public IEnumerator<TNode> GetEnumerator() => nodes.AsEnumerable().GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
