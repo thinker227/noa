0e4f9fa3b25a1208a74d96eb8a76e9831f1f8ad1
Thinker <thinker0227@gmail.com>
2025-02-16 23:46:21 +0100

    Switch IfExpressionTests over to snapshot testing

--- src/compiler-tests/Parsing/IfExpressionTests.cs
+++ src/compiler-tests/Parsing/IfExpressionTests.cs
@@ -1,193 +1,28 @@
-using Noa.Compiler.Syntax.Green;
-using TokenKind = Noa.Compiler.Syntax.TokenKind;
-
 namespace Noa.Compiler.Parsing.Tests;
 
 public sealed class IfExpressionTests
 {
     [Fact]
-    public void Else_IsRequired_InExpressions()
-    {
-        var p = ParseAssertion.Create(
+    public Task Else_IsRequired_InExpressions() =>
+        ParseTest.Test(
             "if true {}",
             p => p.ParseExpressionOrError());
 
-        p.N<IfExpressionSyntax>();
-        p.T(TokenKind.If);
-        p.D(ParseDiagnostics.ElseOmitted.Id);
-        {
-            p.N<BoolExpressionSyntax>();
-            {
-                p.T(TokenKind.True);
-            }
-
-            p.N<BlockExpressionSyntax>();
-            {
-                p.T(TokenKind.OpenBrace);
-                
-                p.N<BlockSyntax>();
-                {
-                    p.N<SyntaxList<StatementSyntax>>();
-                }
-
-                p.T(TokenKind.CloseBrace);
-            }
-
-            p.N<ElseClauseSyntax>();
-            {
-                p.E(); // else
-                
-                p.N<BlockExpressionSyntax>();
-                {
-                    p.E(); // {
-
-                    p.N<BlockSyntax>();
-                    {
-                        p.N<SyntaxList<StatementSyntax>>();
-                    }
-
-                    p.E(); // }
-                }
-            }
-        }
-
-        p.End();
-    }
-
     [Fact]
-    public void Else_IsNotRequired_InStatements()
-    {
-        var p = ParseAssertion.Create(
+    public Task Else_IsNotRequired_InStatements() =>
+        ParseTest.Test(
             "if true {}",
             p => p.ParseRoot());
-
-        p.N<RootSyntax>();
-        {
-            p.N<BlockSyntax>();
-            {
-                p.N<SyntaxList<StatementSyntax>>();
-                {
-                    p.N<FlowControlStatementSyntax>();
-                    {
-                        p.N<IfExpressionSyntax>();
-                        p.T(TokenKind.If);
-                        {
-                            p.N<BoolExpressionSyntax>();
-                            {
-                                p.T(TokenKind.True);
-                            }
-
-                            p.N<BlockExpressionSyntax>();
-                            {
-                                p.T(TokenKind.OpenBrace);
-
-                                p.N<BlockSyntax>();
-                                {
-                                    p.N<SyntaxList<StatementSyntax>>();
-                                }
-
-                                p.T(TokenKind.CloseBrace);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        p.T(TokenKind.EndOfFile);
-
-        p.End();
-    }
     
     [Fact]
-    public void Else_IsAllowed_InExpressions()
-    {
-        var p = ParseAssertion.Create(
+    public Task Else_IsAllowed_InExpressions() =>
+        ParseTest.Test(
             "if true {} else {}",
             p => p.ParseExpressionOrError());
-        
-        p.N<IfExpressionSyntax>();
-        p.T(TokenKind.If);
-        {
-            p.N<BoolExpressionSyntax>();
-            {
-                p.T(TokenKind.True);
-            }
-
-            p.N<BlockExpressionSyntax>();
-            {
-                p.T(TokenKind.OpenBrace);
-
-                p.N<BlockSyntax>();
-                {
-                    p.N<SyntaxList<StatementSyntax>>();
-                }
-
-                p.T(TokenKind.CloseBrace);
-            }
-
-            p.N<ElseClauseSyntax>();
-            p.T(TokenKind.Else);
-            {
-                p.N<BlockExpressionSyntax>();
-                {
-                    p.T(TokenKind.OpenBrace);
-
-                    p.N<BlockSyntax>();
-                    {
-                        p.N<SyntaxList<StatementSyntax>>();
-                    }
-
-                    p.T(TokenKind.CloseBrace);
-                }
-            }
-        }
-
-        p.End();
-    }
 
     [Fact]
-    public void IfWithoutElse_InBlock_ParsesAsStatement()
-    {
-        var p = ParseAssertion.Create(
+    public Task IfWithoutElse_InBlock_ParsesAsStatement() =>
+        ParseTest.Test(
             "if true {}",
             p => p.ParseRoot());
-
-        p.N<RootSyntax>();
-        {
-            p.N<BlockSyntax>();
-            {
-                p.N<SyntaxList<StatementSyntax>>();
-                {
-                    p.N<FlowControlStatementSyntax>();
-                    {
-                        p.N<IfExpressionSyntax>();
-                        p.T(TokenKind.If);
-                        {
-                            p.N<BoolExpressionSyntax>();
-                            {
-                                p.T(TokenKind.True);                            
-                            }
-
-                            p.N<BlockExpressionSyntax>();
-                            {
-                                p.T(TokenKind.OpenBrace);
-
-                                p.N<BlockSyntax>();
-                                {
-                                    p.N<SyntaxList<StatementSyntax>>();
-                                }
-
-                                p.T(TokenKind.CloseBrace);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        p.T(TokenKind.EndOfFile);
-
-        p.End();
-    }
 }
