9426fdb55a63d82a81eefdc6755de6179778d341
Thinker <thinker0227@gmail.com>
2025-02-16 23:46:42 +0100

    Switch StatementTests over to snapshot testing

--- src/compiler-tests/Parsing/StatementTests.cs
+++ src/compiler-tests/Parsing/StatementTests.cs
@@ -1,80 +1,16 @@
-using Noa.Compiler.Syntax.Green;
-using TokenKind = Noa.Compiler.Syntax.TokenKind;
-
 namespace Noa.Compiler.Parsing.Tests;
 
 public class StatementTests
 {
     [Fact]
-    public void Parses_AssignmentStatement_WithIdentifier()
-    {
-        var p = ParseAssertion.Create("x = 0;", p => p.ParseRoot());
-
-        p.N<RootSyntax>();
-        {
-            p.N<BlockSyntax>();
-            {
-                p.N<SyntaxList<StatementSyntax>>();
-                {
-                    p.N<AssignmentStatementSyntax>();
-                    {
-                        p.N<IdentifierExpressionSyntax>();
-                        {
-                            p.T(TokenKind.Name, t => t.Text.ShouldBe("x"));
-                        }
-
-                        p.T(TokenKind.Equals);
-
-                        p.N<NumberExpressionSyntax>();
-                        {
-                            p.T(TokenKind.Number, t => t.Text.ShouldBe("0"));
-                        }
-
-                        p.T(TokenKind.Semicolon);
-                    }
-                }
-            }
-        }
-
-        p.T(TokenKind.EndOfFile);
-
-        p.End();
-    }
+    public Task Parses_AssignmentStatement_WithIdentifier() =>
+        ParseTest.Test(
+            "x = 0;",
+            p => p.ParseRoot());
 
     [Fact]
-    public void Parses_AssignmentStatement_WithNumber_AndProduces_InvalidLValue()
-    {
-        var p = ParseAssertion.Create("0 = 1;", p => p.ParseRoot());
-
-        p.N<RootSyntax>();
-        {
-            p.N<BlockSyntax>();
-            {
-                p.N<SyntaxList<StatementSyntax>>();
-                {
-                    p.N<AssignmentStatementSyntax>();
-                    {
-                        p.N<NumberExpressionSyntax>();
-                        p.D(ParseDiagnostics.InvalidLValue.Id);
-                        {
-                            p.T(TokenKind.Number, t => t.Text.ShouldBe("0"));
-                        }
-
-                        p.T(TokenKind.Equals);
-
-                        p.N<NumberExpressionSyntax>();
-                        {
-                            p.T(TokenKind.Number, t => t.Text.ShouldBe("1"));
-                        }
-
-                        p.T(TokenKind.Semicolon);
-                    }
-                }
-            }
-        }
-
-        p.T(TokenKind.EndOfFile);
-
-        p.End();
-    }
+    public Task Parses_AssignmentStatement_WithNumber_AndProduces_InvalidLValue() =>
+        ParseTest.Test(
+            "0 = 1;",
+            p => p.ParseRoot());
 }
