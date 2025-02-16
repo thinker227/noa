281ef8cd4a1745912fee7905b012ab0b2b16bf5e
Thinker <thinker0227@gmail.com>
2025-02-16 01:34:18 +0100

    Add PostExpression syntax context

--- src/compiler/Services/Context/SyntaxContext.cs
+++ src/compiler/Services/Context/SyntaxContext.cs
@@ -11,28 +11,32 @@ public enum SyntaxContext
     /// </summary>
     None = 0,
     /// <summary>
-    /// An expression.
+    /// An expression, not including expression statements.
     /// <c>(|</c>, <c>1 + |</c>, <c>let x = |</c>, ...
     /// </summary>
     Expression = 1 << 0,
     /// <summary>
+    /// Immediately following an expression.
+    /// </summary>
+    PostExpression = 1 << 1,
+    /// <summary>
     /// A statement.
     /// <c>let x = 1; |</c>, <c>{|</c>, ...
     /// </summary>
-    Statement = 1 << 1,
+    Statement = 1 << 3,
     /// <summary>
     /// A parameter or variable.
     /// <c>let |</c>, <c>func f(|</c>, <c>(|</c>, ...
     /// </summary>
-    ParameterOrVariable = 1 << 2,
+    ParameterOrVariable = 1 << 4,
     /// <summary>
     /// Immediately following the end of the body of an if-statement without an else clause.
     /// <c>if x {} |</c>
     /// </summary>
-    PostIfBodyWithoutElse = 1 << 3,
+    PostIfBodyWithoutElse = 1 << 5,
     /// <summary>
     /// Any level of nesting inside a loop.
     /// <c>loop { |</c>
     /// </summary>
-    InLoop = 1 << 4,
+    InLoop = 1 << 6,
 }
