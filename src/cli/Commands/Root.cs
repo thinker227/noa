ca37dd2b88ba68c70446b56abb75f58c964c243a
Thinker <thinker0227@gmail.com>
2025-04-22 00:29:33 +0200

    Update some formatting

--- src/cli/Commands/Root.cs
+++ src/cli/Commands/Root.cs
@@ -153,16 +153,14 @@ public sealed class Root(
 
         const string runtimePathEnvVar = "NOA_RUNTIME";
         var envRuntimePath = Environment.GetEnvironmentVariable(runtimePathEnvVar);
-
+        var envVarIsSet = false;
         if (envRuntimePath is not null)
         {
+            envVarIsSet = true;
+
             var envProvidedRuntime = new FileInfo(envRuntimePath);
 
-            if (!envProvidedRuntime.Exists)
-            {
-                console.MarkupLine($"[yellow]The environment variable [/][white]{runtimePathEnvVar}[/][yellow] is set, but it doesn't refer to a valid file.[/]");
-            }
-            else return envProvidedRuntime;
+            if (envProvidedRuntime.Exists) return envProvidedRuntime;
         }
 
         var siblingRuntimeName = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
@@ -175,11 +173,19 @@ public sealed class Root(
 
         if (!siblingRuntime.Exists)
         {
+            var envVarMessage = envVarIsSet
+                ? ", [yellow]which was set but did not refer to a valid file.[/]"
+                : ".";
+
             console.MarkupLine($"""
-                [red]Cannot find the Noa runtime. Tried to find the runtime in the following places:
-                  - A path provided by the [/][white]--runtime|-r[/][red] CLI option.
-                  - A path provided by the [/][white]NOA_RUNTIME[/][red] environment variable.
-                  - [/][white]{siblingRuntimePath}[/][red], which did not exist.
+                [red]Cannot find the Noa runtime. Tried to find the runtime in the following places (in this order):
+                  - A path provided by the [white]--runtime|-r[/] CLI option.
+                  - A path provided by the [white]NOA_RUNTIME[/] environment variable{envVarMessage}
+                  - [white]{siblingRuntimePath}[/].
+                
+                By default, the runtime should be located at [white]{siblingRuntimePath}[/].
+                If running in a development environment, specify the path manually or set the environment variable.
+                There's probably a runtime around here somewhere...
                 [/]
                 """);
             
