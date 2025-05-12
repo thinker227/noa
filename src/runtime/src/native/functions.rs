de06d8ab359540bce6049b41259562dc29f331bf
Thinker <thinker0227@gmail.com>
2025-05-12 17:10:29 +0200

    Implement more functions

--- src/runtime/src/native/functions.rs
+++ src/runtime/src/native/functions.rs
@@ -1,7 +1,7 @@
+use std::fs;
 use std::io::Write;
 
 use crate::exception::Exception;
-use crate::heap::HeapValue;
 use crate::vm::{Vm, Result};
 use crate::value::Value;
 
@@ -10,8 +10,11 @@ use super::NativeFunction;
 /// Gets a vector of native functions.
 pub fn get_functions() -> Vec<NativeFunction> {
     vec![
-        print,
-        get_input
+        /* 0x0 */ print,
+        /* 0x1 */ get_input,
+        /* 0x2 */ read_file,
+        /* 0x3 */ write_file,
+        /* 0x4 */ to_string
     ]
 }
 
@@ -56,7 +59,75 @@ fn get_input(vm: &mut Vm, _: Vec<Value>) -> Result<Value> {
     // Pop the trailing newline character
     buf.pop();
 
-    let address = vm.alloc_heap_value(HeapValue::String(buf))?;
+    vm.alloc_string(buf)
+}
 
-    Ok(Value::Object(address))
+fn read_file(vm: &mut Vm, args: Vec<Value>) -> Result<Value> {
+    let path = match args[..] {
+        [] => return Err(vm.exception(
+            Exception::BadArity { expected: 1, or_more: false, actual: 0 }
+        )),
+
+        [path, ..] => vm.to_string(path)?,
+    };
+
+    // let contents = fs::read_to_string(path.clone())
+    //     .map_err(|e| vm.exception(
+    //         Exception::Custom(match e.kind() {
+    //             std::io::ErrorKind::NotFound => format!("could not find file {path}"),
+    //             std::io::ErrorKind::PermissionDenied => format!("permission to access {path} was defined"),
+    //             std::io::ErrorKind::IsADirectory => format!("{path} is a directory, expected a file"),
+    //             std::io::ErrorKind::ReadOnlyFilesystem => format!("file system of {path} is read-only"),
+    //             std::io::ErrorKind::InvalidFilename => format!("invalid file name of {path}"),
+    //             std::io::ErrorKind::OutOfMemory => format!("failed to read file {path}, out of memory"),
+    //             _ => format!("failed to read file {path}"),
+    //         })
+    //     ))?;
+
+    match fs::read_to_string(path) {
+        Ok(content) => vm.alloc_string(content),
+        Err(_) => Ok(Value::Nil),
+    }
+}
+
+fn write_file(vm: &mut Vm, args: Vec<Value>) -> Result<Value> {
+    let arg_count = args.len() as u32;
+    let (path, content) = match args[..] {
+        [] | [_] => return Err(vm.exception(
+            Exception::BadArity { expected: 2, or_more: false, actual: arg_count }
+        )),
+
+        [path, content, ..] => (vm.to_string(path)?, vm.to_string(content)?),
+    };
+
+    // fs::write(path.clone(), content)
+    //     .map_err(|e| vm.exception(
+    //         Exception::Custom(match e.kind() {
+    //             std::io::ErrorKind::NotFound => format!("could not find file {path}"),
+    //             std::io::ErrorKind::PermissionDenied => format!("permission to access {path} was defined"),
+    //             std::io::ErrorKind::IsADirectory => format!("{path} is a directory, expected a file"),
+    //             std::io::ErrorKind::ReadOnlyFilesystem => format!("file system of {path} is read-only"),
+    //             std::io::ErrorKind::InvalidFilename => format!("invalid file name of {path}"),
+    //             std::io::ErrorKind::OutOfMemory => format!("failed to write to file {path}, out of memory"),
+    //             _ => format!("failed to write to file {path}"),
+    //         })
+    //     ))?;
+
+    match fs::write(path, content) {
+        Ok(()) => Ok(Value::Bool(true)),
+        Err(_) => Ok(Value::Bool(false))
+    }
+}
+
+fn to_string(vm: &mut Vm, args: Vec<Value>) -> Result<Value> {
+    match args[..] {
+        [] => {
+            vm.alloc_string(String::from(""))
+        },
+
+        [val, ..] => {
+            let str = vm.to_string(val)?;
+            vm.alloc_string(str)
+        }
+    }
 }
